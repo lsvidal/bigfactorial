e920097ac33d18e766d3627f9f6a1904450c8261
Leonardo Vidal <leonardo73vidal@gmail.com>
2015-11-09 16:04:20 -0200

    Feita versão com cache de instâncias da struct para reduzir o custo dos ciclos de malloc/free.

--- main.c
+++ main.c
@@ -22,6 +22,30 @@ void kill(num *a) {
     }
 }
 
+num *cache = NULL;
+
+num *get(char v, num *n) {
+    num *r;
+    if (cache) {
+        r = cache;
+        cache = cache->n;
+    } else {
+        r = malloc(sizeof(num));
+    }
+    r->v = v;
+    r->n = n;
+    return r;
+}
+
+void put(num *a) {
+    while (a) {
+        num *b = a->n;
+        a->n = cache;
+        cache = a;
+        a = b;
+    }
+}
+
 num *fact(int N) {
     int i;
     num *a = alloc(1, NULL);
@@ -60,6 +84,44 @@ num *fact(int N) {
     return ri;
 }
 
+num *fact_cache(int N) {
+    int i;
+    num *a = get(1, NULL);
+    for (i = 2 ; i <= N ; i++) {
+        num *ai = a, *p = get(0, NULL), *pi = p, *pc;
+        while (ai) {
+            int c = ai->v * i;
+            pc = pi;
+            while (c > 0) {
+                c += pc->v;
+                pc->v = c % 10;
+                c /= 10;
+                if (!pc->n) pc->n = get(0, NULL);
+                pc = pc->n;
+            }
+            ai = ai->n;
+            if (!pi->n) pi->n = get(0, NULL); // This is necessary to handle situations where 'c' starts as zero
+            pi = pi->n;
+        }
+        put(a);
+        a = p;
+    }
+
+    // Inverts the order of the digits
+    num *ai = a, *r = NULL;
+    while (ai) {
+        r = get(ai->v, r);
+        ai = ai->n;
+    }
+    put(a);
+
+    num *ri = r;
+    // Remove leading zeroes
+    while (ri && ri->v == 0) ri = ri->n;
+
+    return ri;
+}
+
 int main()
 {
     num *r = fact(500);
@@ -74,7 +136,19 @@ int main()
     clock_t start, stop;
     start = clock();
     int i;
-    for (i = 0 ; i < 100 ; i++) fact(500);
+    for (i = 0 ; i < 100 ; i++) {
+        r = fact(500);
+        kill(r);
+    }
+    stop = clock();
+    printf("Took an average of %0.7f seconds\n", (double)(stop - start)/CLOCKS_PER_SEC/100);
+
+    start = clock();
+    for (i = 0 ; i < 100 ; i++) {
+        r = fact_cache(500);
+        put(r);
+    }
+    kill(cache);
     stop = clock();
     printf("Took an average of %0.7f seconds\n", (double)(stop - start)/CLOCKS_PER_SEC/100);
     return 0;
